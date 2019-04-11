863c76d75d6e03985226c11d512ae1b1da23ca13
jeongeunson <42817588+jeongeunson@users.noreply.github.com>
2019-04-12 00:28:05 +0900

    Please help me Kris Oppaya

---
+++ main/java/com/example/hw1/Main6Activity.java
@@ -0,0 +1,19 @@
+package com.example.hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main6Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+
+
+
+
+    }
+}
